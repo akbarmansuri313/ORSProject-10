8bdbcd16b68445365bf64d2d693ac18fb8972a3f
akbarmansuri313 <ak9755530652@gmail.com>
2026-01-09 22:07:07 +0530

    suiiii

---
+++ ORSProject-10/src/main/java/com/rays/ORSProject10application.java
@@ -0,0 +1,14 @@
+package com.rays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ORSProject10application {
+
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ORSProject10application.class, args);
+	}
+}
